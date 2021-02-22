9c79d0836913b1011d9adde6f484fca6beda3db9
Gustavo Pantuza <gustavopantuza@gmail.com>
2021-02-22 15:28:22 -0300

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 # More information on setting values:
 # https://github.com/Sceptre/project/wiki/sceptre-resolver-template
